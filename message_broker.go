94e27f49103b78fcae69725f0e0b9e85221e3f96
spaval <142543558+spaval@users.noreply.github.com>
2025-01-29 20:13:52 -0500

    Update message_broker.go
    Add fail channel to Consume function

--- message_broker.go
+++ message_broker.go
@@ -2,5 +2,5 @@ package messagebroker
 
 type MessageBroker interface {
 	Publish(queueName string, message any) error
-	Consume(queueName string, data chan any) error
+	Consume(queueName string, data chan any, fail chan error) error
 }
